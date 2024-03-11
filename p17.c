832afb2203fd00e658b4079ba24b18e41b3f3dda
nik200503 <158718638+nik200503@users.noreply.github.com>
2024-03-12 01:26:31 +0530

    Add files via upload

---
+++ p17.c
@@ -0,0 +1,45 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+typedef struct {
+    int data;
+    struct linked_list *next;
+}linked_list;
+
+int main(){
+    linked_list *head=NULL;
+    linked_list *one=NULL;
+    linked_list *two=NULL;
+    linked_list *three=NULL;
+
+    one=(linked_list*)malloc(sizeof(linked_list));
+    two=(linked_list*)malloc(sizeof(linked_list));
+    three=(linked_list*)malloc(sizeof(linked_list));
+
+    one->data=3;
+    two->data=5;
+    three->data=7;
+
+    one->next=two;
+    two->next=three;
+    three->next=NULL;
+
+    head=one;
+
+    int choice;
+    printf("press 0 for insertion and 1 for deletion");
+    scanf("%d",&choice);
+
+    if(choice==0){ //insertion at THE END
+        linked_list *new_node=NULL;
+        new_node=(linked_list*)malloc(sizeof(linked_list));
+        new_node->data=1;
+        new_node->next=NULL;
+        three->next=new_node;
+    }
+    else if(choice==1){//deletion aT THE END
+        two->next=NULL;
+
+        free(three);
+    }
+}
